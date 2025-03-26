480e1e920bb8fc9af327c0d1c600cb14425a2b63
usn757 <usn757@gmail.com>
2025-03-26 12:45:11 +0900

    git commit -m "Implement v1.1.0 updates (queries)"

--- dmlTest.sql
+++ dmlTest.sql
@@ -24,3 +24,15 @@ INSERT INTO reviews (user_id, product_id, rating, comment) VALUES
 (1, 3, 3, 'Average quality.'),
 (3, 4, 5, 'Perfect fit!'),
 (4, 5, 4, 'Interesting book.');
+
+-- 추가
+INSERT INTO reviews (user_id, product_id, rating, comment) VALUES
+(2, 1, 4, '품질이 좋습니다'),
+(3, 2, 5, '기대 이상의 제품이에요!'),
+(4, 3, 4, '배송이 빠르고 상품도 좋아요'),
+(1, 4, 3, '평범한 제품입니다'),
+(2, 3, 1, '최악의 제품입니다. 돈 낭비였어요'),
+(3, 1, 5, '완벽한 제품입니다'),
+(4, 4, 1, '사용할 수 없을 정도로 품질이 나쁩니다'),
+(1, 5, 4, '재구매 의사 있습니다'),
+(3, 5, 1, '전혀 기대에 미치지 못합니다');
